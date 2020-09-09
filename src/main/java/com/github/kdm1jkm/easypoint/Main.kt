31bd5059d5e7504a9c9f4e0532d618adc7f2a50c
kdm <kdm1jkmm@gmail.com>
2020-09-09 20:43:43 +0900

    Convert Main.java --> Main.kt
    Java --> kotlin
    + refactoring

---
+++ src/main/java/com/github/kdm1jkm/easypoint/Main.kt
@@ -0,0 +1,10 @@
+package com.github.kdm1jkm.easypoint
+
+import com.github.kdm1jkm.easypoint.cli.CLIManager
+
+object Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val manager = CLIManager(args)
+    }
+}
