7a0084db9a00e702367be5adc34fb9897562f998
kdm <kdm1jkmm@gmail.com>
2020-09-12 12:40:51 +0900

    Convert TemplateSlideInfo.java --> TemplateSlideInfo.kt
    Java --> kotlin
    + refactoring

--- src/main/java/com/github/kdm1jkm/easypoint/core/TemplateSlideInfo.kt
+++ src/main/java/com/github/kdm1jkm/easypoint/core/TemplateSlideInfo.kt
@@ -1,50 +1,22 @@
-package com.github.kdm1jkm.easypoint.core;
+package com.github.kdm1jkm.easypoint.core
 
-import org.json.simple.JSONArray;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*
 
 /**
- * {@link TemplateSlide}객체에서 정보를 요청했을 시 반환되는 클래스.
+ * [TemplateSlide]객체에서 정보를 요청했을 시 반환되는 클래스.
+ *
+ * @param parent 원래 [TemplateSlide] 객체의 참조
  */
-public class TemplateSlideInfo {
+class TemplateSlideInfo(val parent: TemplateSlide) {
     /**
      * 슬라이드의 이름
      */
-    public final String name;
+    val name: String = parent.name
+
     /**
      * 텍스트 상자의 이름 목록
      */
-    public final List<String> values = new ArrayList<>();
-    /**
-     * 정보를 담고 있는 원본 {@link TemplateSlide}의 참조.
-     */
-    public final TemplateSlide parent;
+    val values: MutableList<String> = ArrayList()
 
-    /**
-     * 기본 생성자. 원본 클래스의 참조를 가지고 있다.
-     *
-     * @param parent 원본 클래스 {@link TemplateSlide}
-     */
-    public TemplateSlideInfo(TemplateSlide parent) {
-        this.name = parent.name;
-        this.parent = parent;
-    }
-
-    /**
-     * 정보를 JSON형식으로 변환해 반환한다.
-     *
-     * @return 변환된 정보
-     */
-    public JSONArray getJSON() {
-        JSONArray array = new JSONArray();
-        array.addAll(values);
-
-        JSONArray result = new JSONArray();
-        result.add(name);
-        result.add(array);
-
-        return result;
-    }
 }
